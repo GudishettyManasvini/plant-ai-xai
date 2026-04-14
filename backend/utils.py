69fb5e30c635167b2437aafae8f4ef577ce6eff0
GudishettyManasvini <gudishetty.manasvini@gmail.com>
2026-04-14 17:58:41 +0530

    add runtime python version fix

--- backend/utils.py
+++ backend/utils.py
@@ -15,7 +15,7 @@ model = tf.keras.models.load_model("models/plant_model.h5")
 # -----------------------------
 # LOAD CLASS NAMES (FIXED)
 # -----------------------------
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+BASE_DIR = os.path.dirname(__file__)
 json_path = os.path.join(BASE_DIR, "class_names.json")
 
 with open(json_path, "r") as f:
